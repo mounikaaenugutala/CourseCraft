afe1f8f90be505b4263ce97e0ff15bcee060308c
mouni@Mounika <aenugutalamounika@gmail.com>
2026-09-24 14:14:33 +0530

    render

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:3000/api';
+const API_URL = 'https://coursecraft-pj4a.onrender.com/api';
 
 export async function apiRequest(path, method = 'GET', data = null, token = '') {
   const headers = { 'Content-Type': 'application/json' };
